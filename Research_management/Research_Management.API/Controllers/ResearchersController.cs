52584a2d68e358f26faf7bd922e83f1b83cbb4ee
Emmanuel Arenilla Mendoza <EmmanuelArenilla@comfama.com.co>
2023-09-07 14:45:46 -0500

    All methods of the researchers controller are terminated.

--- Research_management/Research_Management.API/Controllers/ResearchersController.cs
+++ Research_management/Research_Management.API/Controllers/ResearchersController.cs
@@ -1,6 +1,67 @@
-﻿namespace Research_management.API.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Research_management.API.Data;
+using Research_management.Shared.Entities;
+
+namespace Research_management.API.Controllers
 {
-    public class ResearchersController
+    [ApiController]
+    [Route("api/researchers")]
+    public class ResearchersController:ControllerBase
     {
+        private readonly DataContext _dataContext = null!;
+
+        public ResearchersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        //Get a list of researchers.
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _dataContext.Researchers.ToListAsync());
+        }
+
+        //Insert a researchers.
+        [HttpPost]
+        public async Task<ActionResult> Post(Researcher researcher)
+        {
+            _dataContext.Researchers.Add(researcher);
+                await _dataContext.SaveChangesAsync();
+            return Ok(researcher);
+        }
+
+        //Get a researchers for id.
+        [HttpGet("{id}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var Researcher = await _dataContext.Researchers.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (Researcher == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(Researcher);
+        }
+
+        //Insert a researchers for id.
+        [HttpDelete("{Id}")]
+        public async Task<ActionResult> Delete(int Id)
+        {
+            var Researcher = await _dataContext.Researchers.FirstOrDefaultAsync(x => x.Id == Id);
+
+            if (Researcher == null)
+            {
+
+                return NotFound();
+            }
+
+            _dataContext.Remove(Researcher);
+
+            await _dataContext.SaveChangesAsync();
+            return Ok(Researcher);
+        }
     }
 }
